436c83e1bd4036d7b64a431330df7860a623ef07
ghdehrl12345 <ghdehrl12345@gmail.com>
2025-12-19 11:52:35 +0900

    메인 함수 구현

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
 )
 
 // 증명 구조체 정의
@@ -17,4 +23,22 @@ func (circuit *AgeCircuit) Define(api frontend.API) error {
 	api.AssertIsLessOrEqual(circuit.LimitAge, myAge)
 
 	return nil
+}
+
+// main 함수
+func main() {
+	var circuit AgeCircuit
+	ccs, _ := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
+
+	pk, vk, _ := groth16.Setup(ccs)
+
+	pkFile, _ := os.Create("zage.pk")
+	pk.WriteTo(pkFile)
+	pkFile.Close()
+
+	vkFile, _ := os.Create("zage.vk")
+	vk.WriteTo(vkFile)
+	vkFile.Close()
+
+	fmt.Println("zage.pk, zage.vk 생성 완료")
 }
